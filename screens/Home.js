db70ffecad99e13c1ce0a762d8bd7ab02d27e057
Nur mizwari <nurmizwari04@gmail.com>
2023-06-14 13:17:41 +0700

    tambah image

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,10 @@ import {
   View,
   StatusBar,
   Image,
+  TextInput,
   FlatList,
+  ScrollView,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -56,7 +59,7 @@ export default function Home() {
         backgroundColor: "#61dafb",
         height: 70,
         width: 150,
-        marginTop: 10,
+        marginTop: 2,
         borderRadius: 5,
         marginRight: 5,
         shadowColor: "black",
@@ -66,42 +69,92 @@ export default function Home() {
         alignItems: "center",
       }}
     >
-      <Text style={{ color: "white" }}>{item.Warung}</Text>
-      <Text style={{ color: "white" }}>{item.alamat}</Text>
-      <Text style={{ color: "white" }}>stock:{item.stock}</Text>
+      <Text style={{ color: "black", fontWeight: 700 }}>{item.Warung}</Text>
+      <Text style={{ color: "black" }}>{item.alamat}</Text>
+
+      <Text
+        style={{
+          color: "black",
+          backgroundColor: "#FFEEBB",
+          padding: 3,
+          borderRadius: 5,
+          fontWeight: 700,
+        }}
+      >
+        Stock:{item.stock}
+      </Text>
     </View>
   );
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar animated={true} backgroundColor="#61dafb" />
       <Image
-        style={{ marginTop: 10, height: "30%" }}
-        source={require("../assets/cart.png")}
+        style={{ marginTop: 10 }}
+        source={require("../assets/wallet.gif")}
       />
       <View
         style={{
-          backgroundColor: "grey",
+          backgroundColor: "#61dafb",
           borderRadius: 5,
           padding: 10,
           marginTop: 10,
           elevation: 3,
         }}
       >
-        <Text style={{ color: "white", fontSize: 16, elevation: 3 }}>
+        <Text
+          style={{
+            color: "black",
+            fontWeight: 700,
+            fontSize: 16,
+            elevation: 3,
+          }}
+        >
           Stock Saleh Burido Pesahangan : 100 Bks
         </Text>
       </View>
-      <Text style={{ color: "black", marginVertical: 10, fontSize: 18 }}>
+      <Text
+        style={{
+          color: "black",
+          fontWeight: 700,
+          marginVertical: 10,
+          fontSize: 18,
+        }}
+      >
         Warung Langganan
       </Text>
       <FlatList
         horizontal
         data={DATA}
+        // numColumns={2} // Jumlah kolom dalam grid
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-    </View>
+      <View style={{ flex: 1, marginVertical: 10 }}>
+        <Text
+          style={{
+            color: "black",
+            fontWeight: 700,
+            marginVertical: 10,
+            fontSize: 18,
+          }}
+        >
+          Pesan Saleh
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Bungkus"
+          // value={email}
+          // onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "white", fontWeight: 700, fontSize: 14 }}>
+            Kirim
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -109,11 +162,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // justifyContent: "center",
-    alignItems: "center",
+    // alignItems: "center",
     padding: 10,
     // backgroundColor: "#F5FCFF",
   },
   warung: {
     fontSize: 18,
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  button: {
+    height: 40,
+    width: "100%",
+    // borderColor: "blue",
+    elevation: 3,
+    backgroundColor: "#61dafb",
+    // borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
